c8a334de7a3da07a26fa914beee01da022472a4a
XX\a <tabsaci@gmail.com>
2022-04-02 09:57:17 +0200

    optParser -> argParser

--- src/unicorn.py
+++ src/unicorn.py
@@ -26,63 +26,63 @@ from unicorn.svg_parser import SvgParser
 class MyEffect(inkex.Effect):
   def __init__(self):
     inkex.Effect.__init__(self)
-    self.OptionParser.add_option("--pen-up-angle",
-                      action="store", type="float",
+    self.arg_parser.add_argument("--pen-up-angle",
+                      action="store", type=float,
                       dest="pen_up_angle", default="50.0",
                       help="Pen Up Angle")
-    self.OptionParser.add_option("--pen-down-angle",
-                      action="store", type="float",
+    self.arg_parser.add_argument("--pen-down-angle",
+                      action="store", type=float,
                       dest="pen_down_angle", default="30.0",
                       help="Pen Down Angle")
-    self.OptionParser.add_option("--start-delay",
-                      action="store", type="float",
+    self.arg_parser.add_argument("--start-delay",
+                      action="store", type=float,
                       dest="start_delay", default="150.0",
                       help="Delay after pen down command before movement in milliseconds")
-    self.OptionParser.add_option("--stop-delay",
-                      action="store", type="float",
+    self.arg_parser.add_argument("--stop-delay",
+                      action="store", type=float,
                       dest="stop_delay", default="150.0",
                       help="Delay after pen up command before movement in milliseconds")
-    self.OptionParser.add_option("--xy-feedrate",
-                      action="store", type="float",
+    self.arg_parser.add_argument("--xy-feedrate",
+                      action="store", type=float,
                       dest="xy_feedrate", default="3500.0",
                       help="XY axes feedrate in mm/min")
-    self.OptionParser.add_option("--z-feedrate",
-                      action="store", type="float",
+    self.arg_parser.add_argument("--z-feedrate",
+                      action="store", type=float,
                       dest="z_feedrate", default="150.0",
                       help="Z axis feedrate in mm/min")
-    self.OptionParser.add_option("--z-height",
-                      action="store", type="float",
+    self.arg_parser.add_argument("--z-height",
+                      action="store", type=float,
                       dest="z_height", default="0.0",
                       help="Z axis print height in mm")
-    self.OptionParser.add_option("--finished-height",
-                      action="store", type="float",
+    self.arg_parser.add_argument("--finished-height",
+                      action="store", type=float,
                       dest="finished_height", default="0.0",
                       help="Z axis height after printing in mm")
-    self.OptionParser.add_option("--register-pen",
-                      action="store", type="string",
+    self.arg_parser.add_argument("--register-pen",
+                      action="store", type=str,
                       dest="register_pen", default="true",
                       help="Add pen registration check(s)")
-    self.OptionParser.add_option("--x-home",
-                      action="store", type="float",
+    self.arg_parser.add_argument("--x-home",
+                      action="store", type=float,
                       dest="x_home", default="0.0",
                       help="Starting X position")
-    self.OptionParser.add_option("--y-home",
-                      action="store", type="float",
+    self.arg_parser.add_argument("--y-home",
+                      action="store", type=float,
                       dest="y_home", default="0.0",
                       help="Starting Y position")
-    self.OptionParser.add_option("--num-copies",
-                      action="store", type="int",
+    self.arg_parser.add_argument("--num-copies",
+                      action="store", type=int,
                       dest="num_copies", default="1")
-    self.OptionParser.add_option("--continuous",
-                      action="store", type="string",
+    self.arg_parser.add_argument("--continuous",
+                      action="store", type=str,
                       dest="continuous", default="false",
                       help="Plot continuously until stopped.")
-    self.OptionParser.add_option("--pause-on-layer-change",
-                      action="store", type="string",
+    self.arg_parser.add_argument("--pause-on-layer-change",
+                      action="store", type=str,
                       dest="pause_on_layer_change", default="false",
                       help="Pause on layer changes.")
-    self.OptionParser.add_option("--tab",
-                      action="store", type="string",
+    self.arg_parser.add_argument("--tab",
+                      action="store", type=str,
                       dest="tab")
 
   def output(self):
